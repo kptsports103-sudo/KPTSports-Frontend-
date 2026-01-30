11d9e290b8d1de3b9e9ae76bc579bbea2211ea08
kptsports103-sudo <kptsports103@gmail.com>
2026-01-30 10:18:21 +0530

    update Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -149,24 +149,6 @@ export default function Home() {
 
 
 
-      {/* Footer */}
-      <div
-        style={{
-          position: 'fixed',
-          bottom: 0,
-          left: 0,
-          width: '100%',
-          textAlign: 'center',
-          fontSize: '14px',
-          color: '#fff',
-          background: '#0f3b2e',
-          padding: '12px 0',
-          zIndex: 1000,
-        }}
-      >
-        © 2023 KPT Sports. All rights reserved.
-      </div>
-
       <StudentParticipationModal
         isOpen={studentModalOpen}
         onClose={() => setStudentModalOpen(false)}
